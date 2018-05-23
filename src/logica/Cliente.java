862086f807bdfe1d2b1c8e4412aaa718d7609865
Eileen Guerrero <eileenguerrerogomez@gmail.com>
2018-05-23 11:12:53 -0500

    asjasjdjssd

--- src/logica/Cliente.java
+++ src/logica/Cliente.java
@@ -1,7 +1,48 @@
 package logica;
 
+import java.io.EOFException;
+import java.io.IOException;
 
 public class Cliente {
+	
+	
+	public Cliente () {
+		 try {
+	         conectarAServidor(); // Paso 1: crear un socket para realizar la conexión
+	         obtenerFlujos();      // Paso 2: obtener los flujos de entrada y salida
+	         procesarConexion(); // Paso 3: procesar la conexión
+	      }
+	      // el servidor cerró la conexión
+	      catch ( EOFException excepcionEOF ) {
+	         System.err.println( "El cliente termino la conexión" );
+	      }
+	      // procesar los problemas que pueden ocurrir al comunicarse con el servidor
+	      catch ( IOException excepcionES ) {
+	         excepcionES.printStackTrace();
+	      }
+
+	      finally {
+	         cerrarConexion(); // Paso 4: cerrar la conexión
+	      }
+	}
+	
+	
+	public void conectarAServidor() {
+		
+	}
+	
+	
+	public void obtenerFlujos() {
+		
+	}
+	
+	public void procesarConexion() {
+		
+	}
+	
+	public void cerrarConexion() {
+		
+	}
 	
 
 }
